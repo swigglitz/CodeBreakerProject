74eceb4fec7b1649442681fcb3197d5a34bc0bd3
Hicks <Andy@wlv.ac.uk>
2017-05-17 11:54:56 +0100

    getResults

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,39 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
+//let thisMessage = document.getElementById('message');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if(answer == '' && attempt == ''){
+        setHiddenFields();
+    }
+    if(!validateInput(input.value)) {
+        return false;
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+    answer = Math.floor(Math.random() * 10000 ).toString();
+    while(answer.length < 4){
+        answer = '0' + answer.value;
+    }
+    attempt = 0; 
+}
+
+function setMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input){
+    if (input.length = 4){
+        return true;
+    } else {
+        setMessage("Guesses must be exactly 4 charcters long");
+        return false;
+    }
+}
+
+function getResults(guess) {
+    let originalGuess = guess;
+    document.getElementById('results').innerHTML("<div class='row'>");
+}
